bfde5cd94e61fb0c61576df77e828040699dffe2
osadeyemo <105608425+osadeyemo@users.noreply.github.com>
2023-05-18 18:23:41 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform{
         region = "us-east-1"
         profile = "default"
         key = "terraformstatefile"
-        bucket = "terraformgitlab2022"
+        bucket = "gitlabbucket2023"
     }
 }
